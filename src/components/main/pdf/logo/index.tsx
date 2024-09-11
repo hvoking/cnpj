49790620f9c6ce8f7fb07db7acc221ed82eb159a
hvoking <gus@ugeom.com>
2024-09-10 20:06:15 -0400

    backup 10-09-24

--- src/components/main/pdf/logo/index.tsx
+++ src/components/main/pdf/logo/index.tsx
@@ -3,10 +3,10 @@ export const Logo = () => {
 		<div className="logo-wrapper" style={{paddingBottom: "20px", paddingLeft: "5px", paddingTop: "10px"}}>
 			<img 
 				className="logo"
-				src="static/logos/pink.svg" 
+				src={process.env.PUBLIC_URL + "/static/logos/black.svg"} 
 				alt="header-logo"
 			/>
-			<div className="logo-name" style={{paddingLeft: "8px", color: "rgba(0, 0, 0, 1)"}}>Airbnb Metrics</div>
+			<div className="logo-name" style={{paddingLeft: "8px", color: "rgba(0, 0, 0, 1)"}}>Spatial Fingers</div>
 		</div>
 	)
 }
